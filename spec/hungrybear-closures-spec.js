c3eb6adf70faa6c78648fd3442933e7ea34c69ca
Stuart Gill <stuart.a.gill@gmail.com>
2019-01-13 19:08:48 -0800

    added paralell files using closures

---
+++ spec/hungrybear-closures-spec.js
@@ -0,0 +1,19 @@
+
+import { bear } from './../src/main-closures.js';
+
+describe('HungryBear', function() {
+  let fuzzy = bear;
+
+  beforeEach(function() {
+    jasmine.clock().install();
+    fuzzy.foodLevel = 10;
+    fuzzy.name = "Fuzzy";
+    fuzzy.setHunger();
+  });
+
+  it('should return that the bear ate blueberries and the food level should go up 5', function() {
+    expect(fuzzy.eatSmall("blueberries")).toEqual("The bear ate the blueberries! Food level goes up 5!");
+    expect(fuzzy.foodLevel).toEqual(15);
+  });
+
+});
